e2fba9c8e13f3022dbac81d3da3df40b7bff97fa
g00339839 <g00339839@gmit.ie>
2019-10-29 19:47:35 +0000

    first enemy and gun explosion added

--- Jones Journey/Assets/Scripts/Bullet.cs
+++ Jones Journey/Assets/Scripts/Bullet.cs
@@ -8,17 +8,38 @@ public class Bullet : MonoBehaviour
     private float bulletSpeed;
     [SerializeField]
     private float lifeTime;
-     
+    [SerializeField]
+    private int damage;
+    [SerializeField]
+    private GameObject explosion;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, lifeTime);
+        Invoke("DestroyBullet", lifeTime);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * bulletSpeed * Time.deltaTime);
+    }
+
+   
+
+    private void DestroyBullet()
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.GetComponent<Enemy>().TakeDamage(damage);
+            DestroyBullet();
+        }
     }
 }
---
+++ Jones Journey/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    protected int health;
+    [SerializeField]
+    protected float speed;
+    [HideInInspector]
+    public Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    public void TakeDamage(int damageAmount)
+    {
+
+        health -= damageAmount;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
