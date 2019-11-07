c54553bf824442bbb799dd7857ba04a06d533a24
g00339839 <g00339839@gmit.ie>
2019-11-07 02:04:59 +0000

    Added swat enemy,start shooting

---
+++ Jones Journey/Assets/Scripts/SwatEnemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwatEnemy : Enemy
+{
+    public float stopDistance;
+    private float attackTime;
+    private Animator anim;
+    public Transform shotPoint;
+    public GameObject enemyBullet;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            if (Vector2.Distance(transform.position, player.position) > stopDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+            if (Time.time >= attackTime)
+            {
+
+                attackTime = Time.time + timeBetweenAttack;
+                anim.SetTrigger("attack");
+            }
+        }
+
+    }
+    public void SwatAttack()
+    {
+        Vector2 direction = player.position - shotPoint.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        shotPoint.rotation = rotation;
+        Instantiate(enemyBullet, shotPoint.position, shotPoint.transform.rotation);
+
+    }
+    }
